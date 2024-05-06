3b26fc5819bd5819a89cfda60daf5e7dea1c3d79
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-05-06 11:24:19 +0700

    add class pelajar dan manusia beserta member

--- FriendFunction2/FriendFunction2.cpp
+++ FriendFunction2/FriendFunction2.cpp
@@ -16,16 +16,5 @@ public:
     friend void manusia::showNilaiPelajar(pelajar &x);
 };
 
-void manusia::showNilaiPelajar(pelajar& x) {
-    std::cout << x.nilai;
-}
 
-
-int main()
-{
-    manusia budi;
-    pelajar pbudi;
-    budi.showNilaiPelajar(pbudi);
-    return 0;
-}
 
